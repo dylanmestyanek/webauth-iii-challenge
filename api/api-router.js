78508872f48d3e1345399b8894b10b98b7452e09
Dylan Mestyanek <mestyanekdylan@gmail.com>
2019-11-13 15:46:20 -0600

    Added restricted middleware that checks to ensure token exists before accessing endpoint

--- api/api-router.js
+++ api/api-router.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 
-const authRouter = require("../routes/auth-router");
+const authRouter = require("../auth/auth-router");
+const userRouter = require("../routes/users-router");
 
 router.use("/auth", authRouter);
+router.use("/users", userRouter);
 
 module.exports = router;
